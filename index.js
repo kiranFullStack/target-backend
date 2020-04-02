adcaacf29abe299ef59e1458d9c247747162c462
kiranFullStack <kiran.fullstack@outlook.com>
2020-04-02 23:28:17 +0530

    removed 000z

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ let data = [
     isActive: "✅",
     price: 100,
     offerPrice: 94,
-    offerStartAt: new Date(2018, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2019, 11, 24, 10, 33, 30, 0),
-    createdAt: new Date(2018, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 10, 24, 10, 33, 30, 0)
+    offerStartAt: new Date(2018, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2019, 11, 24, 10, 33, 30),
+    createdAt: new Date(2018, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 10, 24, 10, 33, 30)
   },
   {
     productName: "Product B",
@@ -21,10 +21,10 @@ let data = [
     isActive: "✅",
     price: 120,
     offerPrice: 92,
-    offerStartAt: new Date(2020, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2029, 11, 24, 10, 33, 30, 0),
-    createdAt: new Date(2028, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 10, 24, 10, 33, 30, 0)
+    offerStartAt: new Date(2020, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2029, 11, 24, 10, 33, 30),
+    createdAt: new Date(2028, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 10, 24, 10, 33, 30)
   },
   {
     productName: "Product C",
@@ -32,10 +32,10 @@ let data = [
     isActive: "✅",
     price: 193,
     offerPrice: 190,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product D",
@@ -43,10 +43,10 @@ let data = [
     isActive: "❌",
     price: 128,
     offerPrice: 89,
-    offerStartAt: new Date(2011, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2011, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2009, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2019, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2011, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2011, 11, 14, 9, 35, 20),
+    createdAt: new Date(2009, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2019, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product E",
@@ -54,10 +54,10 @@ let data = [
     isActive: "❌",
     price: 185,
     offerPrice: 159,
-    offerStartAt: new Date(2010, 11, 24, 12, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 12, 33, 30, 0),
-    updatedAt: new Date(2017, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 12, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 12, 33, 30),
+    updatedAt: new Date(2017, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product F",
@@ -65,10 +65,10 @@ let data = [
     isActive: "✅",
     price: 183,
     offerPrice: 176,
-    offerStartAt: new Date(2012, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2014, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2006, 10, 24, 8, 33, 30, 0),
-    updatedAt: new Date(2017, 12, 25, 10, 29, 35, 0)
+    offerStartAt: new Date(2012, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2014, 11, 14, 9, 35, 20),
+    createdAt: new Date(2006, 10, 24, 8, 33, 30),
+    updatedAt: new Date(2017, 12, 25, 10, 29, 35)
   },
   {
     productName: "Product G",
@@ -76,10 +76,10 @@ let data = [
     isActive: "❌",
     price: 865,
     offerPrice: 797,
-    offerStartAt: new Date(2010, 12, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 12, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 11, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 11, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 12, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 12, 14, 9, 35, 20),
+    createdAt: new Date(2008, 11, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 11, 25, 11, 29, 35)
   },
   {
     productName: "Product H",
@@ -87,10 +87,10 @@ let data = [
     isActive: "❌",
     price: 18,
     offerPrice: 9,
-    offerStartAt: new Date(2005, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2006, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2009, 12, 25, 12, 29, 35, 0)
+    offerStartAt: new Date(2005, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2006, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2009, 12, 25, 12, 29, 35)
   },
   {
     productName: "Product I",
@@ -98,10 +98,10 @@ let data = [
     isActive: "✅",
     price: 86,
     offerPrice: 49,
-    offerStartAt: new Date(2010, 12, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 12, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 12, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 12, 35, 0)
+    offerStartAt: new Date(2010, 12, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 12, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 12, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 12, 35)
   },
   {
     productName: "Product J",
@@ -109,10 +109,10 @@ let data = [
     isActive: "✅",
     price: 56,
     offerPrice: 50,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 9, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 9, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 9, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 9, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 9, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 9, 35)
   },
   {
     productName: "Product K",
@@ -120,10 +120,10 @@ let data = [
     isActive: "❌",
     price: 182,
     offerPrice: 180,
-    offerStartAt: new Date(2010, 4, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 4, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 4, 35, 0)
+    offerStartAt: new Date(2010, 4, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 4, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 4, 35)
   },
   {
     productName: "Product L",
@@ -131,10 +131,10 @@ let data = [
     isActive: "✅",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product M",
@@ -142,10 +142,10 @@ let data = [
     isActive: "✅",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product N",
@@ -153,10 +153,10 @@ let data = [
     isActive: "✅",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product O",
@@ -164,10 +164,10 @@ let data = [
     isActive: "❌",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product P",
@@ -175,10 +175,10 @@ let data = [
     isActive: "❌",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product Q",
@@ -186,10 +186,10 @@ let data = [
     isActive: "❌",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product R",
@@ -197,10 +197,10 @@ let data = [
     isActive: "❌",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product S",
@@ -208,10 +208,10 @@ let data = [
     isActive: "✅",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product T",
@@ -219,10 +219,10 @@ let data = [
     isActive: "❌",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product U",
@@ -230,10 +230,10 @@ let data = [
     isActive: "❌",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product V",
@@ -241,10 +241,10 @@ let data = [
     isActive: "✅",
     price: 8,
     offerPrice: 9,
-    offerStartAt: new Date(2010, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product W",
@@ -252,10 +252,10 @@ let data = [
     isActive: "✅",
     price: 184,
     offerPrice: 94,
-    offerStartAt: new Date(2010, 5, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 5, 9, 35, 20, 0),
-    createdAt: new Date(2008, 5, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2018, 5, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 5, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 5, 9, 35, 20),
+    createdAt: new Date(2008, 5, 24, 10, 33, 30),
+    updatedAt: new Date(2018, 5, 25, 11, 29, 35)
   },
   {
     productName: "Product X",
@@ -263,10 +263,10 @@ let data = [
     isActive: "❌",
     price: 58,
     offerPrice: 49,
-    offerStartAt: new Date(2010, 7, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 7, 9, 35, 20, 0),
-    createdAt: new Date(2008, 10, 24, 7, 33, 30, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 7, 35, 0)
+    offerStartAt: new Date(2010, 7, 24, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 7, 9, 35, 20),
+    createdAt: new Date(2008, 10, 24, 7, 33, 30),
+    updatedAt: new Date(2018, 12, 25, 11, 7, 35)
   },
   {
     productName: "Product Y",
@@ -274,10 +274,10 @@ let data = [
     isActive: "❌",
     price: 866,
     offerPrice: 669,
-    offerStartAt: new Date(2016, 11, 24, 10, 33, 30, 0),
-    offerEndAt: new Date(2016, 11, 14, 9, 35, 20, 0),
-    createdAt: new Date(2016, 10, 24, 10, 33, 30, 0),
-    updatedAt: new Date(2017, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2016, 11, 24, 10, 33, 30),
+    offerEndAt: new Date(2016, 11, 14, 9, 35, 20),
+    createdAt: new Date(2016, 10, 24, 10, 33, 30),
+    updatedAt: new Date(2017, 12, 25, 11, 29, 35)
   },
   {
     productName: "Product Z",
@@ -285,10 +285,10 @@ let data = [
     isActive: "✅",
     price: 865,
     offerPrice: 697,
-    offerStartAt: new Date(2010, 11, 11, 10, 33, 30, 0),
-    offerEndAt: new Date(2010, 11, 14, 9, 11, 20, 0),
-    createdAt: new Date(2008, 10, 24, 10, 33, 11, 0),
-    updatedAt: new Date(2018, 12, 25, 11, 29, 35, 0)
+    offerStartAt: new Date(2010, 11, 11, 10, 33, 30),
+    offerEndAt: new Date(2010, 11, 14, 9, 11, 20),
+    createdAt: new Date(2008, 10, 24, 10, 33, 11),
+    updatedAt: new Date(2018, 12, 25, 11, 29, 35)
   }
 ]
 
